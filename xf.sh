36f6e7dabb8c6be5c03ee6b9df7f4444850164d2
kolekall <97018368+kolekall@users.noreply.github.com>
2022-04-05 16:39:24 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "ed0b368a-6100-42bc-f584-8e26d5db1beb",
                         "alterId": 0
                     }
                 ],
